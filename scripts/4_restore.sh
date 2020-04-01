34474fe2599d10d4b10eeb863b4aa406a9d7bbd7
Adrian Kosmaczewski <adrian@vshn.ch>
2020-04-01 18:32:19 +0200

    Fixing the restoration

--- scripts/4_restore.sh
+++ scripts/4_restore.sh
@@ -10,14 +10,14 @@ source scripts/environment.sh
 eval "$(crc oc-env)"
 
 # Restore WordPress PVC
-restic snapshots --json --last --path /data/wordpress-pvc | python scripts/customize.py wordpress | oc apply -f -
+restic snapshots --json --last --path /data/wordpress-test-wordpress-data | python scripts/customize.py wordpress | oc apply -f -
 
 # Read SQL data from Restic into file
-SNAPSHOT_ID=$(restic snapshots --json --last --path /default-mariadb | jq -r '.[0].id')
+SNAPSHOT_ID=$(restic snapshots --json --last --path /k8up-tutorial-mysql | jq -r '.[0].id')
 restic dump "${SNAPSHOT_ID}" /default-mariadb > backup.sql
 
 # Restore MariaDB data
-MARIADB_POD=$(kubectl get pods | grep mariadb | awk '{print $1}')
+MARIADB_POD=$(oc get pods | grep db | grep Running | awk '{print $1}')
 oc cp backup.sql "$MARIADB_POD":/
 oc cp scripts/db_restore.sh "$MARIADB_POD":/
 oc exec "$MARIADB_POD" -- /db_restore.sh
